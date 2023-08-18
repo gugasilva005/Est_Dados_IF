c15ef930c598af2f32eb0364b3b3661673ad1437
Gustavo Ferreira Silva <130005216+gugasilva005@users.noreply.github.com>
2023-08-18 08:43:20 -0300

    Update questao_10.c
    código final funcional

--- Lista_02/questao_10.c
+++ Lista_02/questao_10.c
@@ -9,25 +9,33 @@ int main(void) {
 
     while (1) {
         scanf("%c", &opcao);
+        setbuf(stdin, NULL); 
         if (tolower(opcao) == 'n') {
+            if (maior_velo == 0 && maior_ano == 0) {
+                printf("zero");
+            }
             break;
         }
 
         scanf("%d", &ano);
+        setbuf(stdin, NULL);
         if (ano > maior_ano) {
-            maior_ano += ano;
+            maior_ano = ano;
         }
 
         scanf("%lf", &velocidade);
-        velo_media += velocidade;
+        setbuf(stdin, NULL);
         if (velocidade > maior_velo) {
-            maior_velo += velocidade;
+            maior_velo = velocidade;
         }
+        velo_media += velocidade;
         cont++;
     }
-    
-    media = velo_media / cont;
-    printf("%.2lf\n%d\n%.2lf", maior_velo, ano, media);
 
+    if (maior_ano > 0 && maior_velo > 0) {
+        media = velo_media / cont;
+        printf("%.2lf\n%d\n%.2lf", maior_velo, maior_ano, media);
+    }
+    
     return 0;
 }
