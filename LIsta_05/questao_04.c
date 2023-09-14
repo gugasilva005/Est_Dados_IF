87198c16b0f792010ed19ec5fc574b9956be2f10
Gustavo Ferreira Silva <130005216+gugasilva005@users.noreply.github.com>
2023-09-14 09:52:54 -0300

    Update questao_04.c

--- LIsta_05/questao_04.c
+++ LIsta_05/questao_04.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#define TAM 100
 
 void troca_letra(char *str, char original, char novo) {
 
+    for (int i = 0; i < TAM; i++) {
+        if (str[i] == original) {
+            str[i] = novo;
+        }
+    }
 }
 
 int main(void) {
 
-    char str[100], original, novo;
+    char str[TAM], original, novo;
 
-    
+    printf("Digite uma palavra: ");
+    fgets(str, TAM, stdin);
+    setbuf(stdin, NULL);
 
+    printf("indique uma letra da palavra escrita: ");
+    scanf("%c", &original);
+    setbuf(stdin, NULL);
+
+    printf("Digite uma nova letra: ");
+    scanf("%c", &novo);
+    setbuf(stdin, NULL);
+
+    troca_letra(str, original, novo);
+
+    printf("%s", str);
 
     return 0;
 }
