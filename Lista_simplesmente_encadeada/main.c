62e76889a4b4ded8f6b5abcf007eefa3b65ed712
Gustavo Ferreira Silva <130005216+gugasilva005@users.noreply.github.com>
2023-10-27 10:35:10 -0300

    Update main.c

--- Lista_simplesmente_encadeada/main.c
+++ Lista_simplesmente_encadeada/main.c
@@ -1,70 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listadinalu.c"
+#include "listadinalu.h"
 
 int main(void) {
-    ListaAluno lista_alunos;
+  ListaAluno lista_alunos;
 
-    criar(&lista_alunos);
+  criar(&lista_alunos);
 
-    int matricula = 100;
-    char nome[30] = "Gustavo";
-    float nota1 = 8.7;
-    float nota2 = 9.5;
+  int matricula = 100;
+  char nome[30] = "Gustavo";
+  float nota1 = 8.7;
+  float nota2 = 9.5;
 
-    if (inserir_aluno(&lista_alunos, matricula, nome, nota1, nota2)) {
-        printf("Aluno inserido com sucesso.\n");
-    } else {
-        printf("Falha ao inserir o aluno.\n");
-    }
+  if (inserir_aluno(&lista_alunos, matricula, nome, nota1, nota2)) {
+    printf("Aluno inserido com sucesso.\n");
+  } else {
+    printf("Falha ao inserir o aluno.\n");
+  }
 
-    int matricula2 = 200;
-    char nome2[30] = "Maria";
-    float nota3 = 9.0;
-    float nota4 = 7.0;
+  int matricula2 = 200;
+  char nome2[30] = "Maria";
+  float nota3 = 9.0;
+  float nota4 = 7.0;
 
-    if (inserir_aluno(&lista_alunos, matricula2, nome2, nota3, nota4)) {
-        printf("Aluno inserido com sucesso.\n");
-    } else {
-        printf("Falha ao inserir o aluno.\n");
-    }    
+  if (inserir_aluno(&lista_alunos, matricula2, nome2, nota3, nota4)) {
+    printf("Aluno inserido com sucesso.\n");
+  } else {
+    printf("Falha ao inserir o aluno.\n");
+  }    
+
+
+  int matricula3 = 300;
+  char nome3[30] = "Josué";
+  float nota5 = 7.5;
+  float nota6 = 8.0;
+
+  if (inserir_aluno(&lista_alunos, matricula3, nome3, nota5, nota6)) {
+    printf("Aluno inserido com sucesso.\n");
+  } else {
+    printf("Falha ao inserir o aluno.\n");
+  }
+
+  printf("\n");
+  exibir_lista(&lista_alunos);
+
+  printf("\n\n");
+
+  char nome_procurar[30] = "Gustavo";
+  exibir_aluno_nome(&lista_alunos, nome_procurar);
     
-
-    int matricula3 = 300;
-    char nome3[30] = "Josué";
-    float nota5 = 7.5;
-    float nota6 = 8.0;
-
-    if (inserir_aluno(&lista_alunos, matricula3, nome3, nota5, nota6)) {
-        printf("Aluno inserido com sucesso.\n");
-    } else {
-        printf("Falha ao inserir o aluno.\n");
-    }
-
-    printf("\n");
-    exibir_lista(&lista_alunos);
-
-    remover(&lista_alunos, 100);
-
-    printf("\n\n\n");
-    exibir_lista(&lista_alunos);
-
-    printf("\n");
-    int matricula4 = 400;
-    char nome4[30] = "Gabriel";
-    float nota7 = 4.0;
-    float nota8 = 3.5;
-
-    if (inserir_aluno(&lista_alunos, matricula4, nome4, nota7, nota8)) {
-        printf("Aluno inserido com sucesso.\n");
-    } else {
-        printf("Falha ao inserir o aluno.\n");
-    }
-
-    printf("\n\n\n");
-    exibir_lista(&lista_alunos);
-    
-
-    
-
 }
